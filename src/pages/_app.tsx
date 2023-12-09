cfc8f89482a2c09c8dcee6b975b3ec333aba545c
Bill Story <storywr@gmail.com>
2023-12-09 14:11:28 -0500

    add spotify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,29 @@
 import { type AppType } from "next/app";
 import { ClerkProvider } from '@clerk/nextjs'
-
+import '@radix-ui/themes/styles.css';
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { useState } from "react";
+import { Theme } from "@radix-ui/themes";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp: AppType = ({ Component, pageProps }: any) => {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
     <ClerkProvider>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Theme>
+            <Component {...pageProps} />
+          </Theme>
+        </Hydrate>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 };
