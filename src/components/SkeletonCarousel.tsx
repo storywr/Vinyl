c2270357383d65eada41e851ad79f96c5c5342cc
Bill Story <storywr@gmail.com>
2023-12-12 23:51:15 -0500

    fix skeleton

--- src/components/SkeletonCarousel.tsx
+++ src/components/SkeletonCarousel.tsx
@@ -1,30 +1,34 @@
 import Skeleton from '@mui/material/Skeleton';
+import { Card } from '@radix-ui/themes';
 import Carousel from "react-multi-carousel"
 
-const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-}
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1
+    }
+  };
 
 const SkeletonCarousel = () => {
   return (
     <Carousel responsive={responsive}>
-      {Array.from({ length: 4 }).map((u, i) => (
-        <Skeleton className='min-h-[500px] md:w-[440px] mx-auto' key={i} sx={{ backgroundColor: '#0f172a' }} />
+      {Array.from({ length: 20 }).map((u, i) => (
+        <div className='flex flex-col bg-transparent max-w-[640px] max-h-[460px] md:max-h-none md:w-[440px] mx-auto'>
+          <Skeleton height={560} width={440} variant='rounded' key={i} sx={{ backgroundColor: '#0f172a' }}>
+            <img className='object-cover' height={640} width={640} />
+          </Skeleton>
+        </div>
       ))}
     </Carousel>
   )
