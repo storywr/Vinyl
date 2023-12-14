06bc4d04877023d0e22ea38e1dc89a0c87974ed0
Bill Story <storywr@gmail.com>
2023-12-14 01:23:06 -0500

    add key to skeleton

--- src/components/SkeletonCarousel.tsx
+++ src/components/SkeletonCarousel.tsx
@@ -24,7 +24,7 @@ const SkeletonCarousel = () => {
   return (
     <Carousel responsive={responsive}>
       {Array.from({ length: 20 }).map((u, i) => (
-        <div className='flex flex-col bg-transparent max-w-[640px] max-h-[460px] md:max-h-none md:w-[440px] mx-auto'>
+        <div key={i} className='flex flex-col bg-transparent max-w-[640px] max-h-[460px] md:max-h-none md:w-[440px] mx-auto'>
           <Skeleton height={560} width={440} variant='rounded' key={i} sx={{ backgroundColor: '#0f172a' }}>
             <img className='object-cover' height={640} width={640} />
           </Skeleton>
